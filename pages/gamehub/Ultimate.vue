923c4ff24cac9fd704b8e6b0dd668f1e1bdff727
jane <janeluciaschoenfeld@gmail.com>
2024-03-11 21:59:17 +0100

    feat: add player names and winning modal

--- pages/gamehub/Ultimate.vue
+++ pages/gamehub/Ultimate.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-screen-md mx-auto flex gap-12 h-full flex-col relative items-center">
+  <div class="max-w-screen-md mx-auto flex gap-12 h-full flex-col relative items-center relative">
     <div class="border-x border-b border-solid border-primary rounded-br-box rounded-bl-box flex flex-col gap-2 w-full">
       <h2 class="font-bold text-2xl sm:text-4xl px-4 bg-primary w-fit rounded-tr-box rounded-br-box">
         Tic Tac Toe - Ultimate
@@ -8,17 +8,17 @@
         class="flex justify-between px-4 text-xl sm:text-2xl"
         :class="{ 'font-bold bg-secondary px-4': player === 'X' }"
       >
-        <p>Player 1</p>
+        <p>{{playerOne}}</p>
         <p>X</p>
       </div>
       <div
         class="flex justify-between px-4 mb-4 text-xl sm:text-2xl"
         :class="{ 'font-bold bg-secondary px-4 ': player === 'O' }"
       >
-        <p>Player 2</p>
+        <p>{{playerTwo}}</p>
         <p>O</p>
       </div>
-      <WinningModal v-if="field.hasWon" />
+      <WinningModal v-if="field.hasWon" :player="field.hasWon === 'X' ? playerOne : playerTwo " @new-game="newGame"/>
     </div>
     <div class="w-[90vw] h-[90vw] sm:w-[50vw] sm:h-[50vw] lg:w-[30vw] lg:h-[30vw]">
       <Gamefield :field="field" :player="player" @clicked-cell="clickedCell" :active-field="activeField" />
@@ -29,11 +29,23 @@
 <script setup lang="ts">
 import { Field } from '~/components/gamefield';
 import confettiModule from 'canvas-confetti/dist/confetti.module.mjs';
+import { ref } from 'vue';
 
 const field = reactive(new Field(() => new Field(() => '')));
 const player = ref<'X' | 'O'>(Math.random > 0.5 ? 'X' : 'O');
-
 const activeField = ref<{ row: number; col: number } | null>(null);
+
+function getItem(item) {
+  console.log(process.client, Date.now())
+  if (process.client) {
+    return localStorage.getItem(item)
+  } else {
+    return null
+  }
+}
+
+const playerOne = ref<string | null>(getItem('playerOne'));
+const playerTwo = ref<string | null>(getItem('playerTwo'));
 
 const clickedCell = (
   subField: Field<string>,
@@ -84,5 +96,10 @@ const clickedCell = (
     }
   }
 
+};
+
+const newGame = () => {
+  console.log('New Game');
+  navigateTo('/gamehub/ultimate');
 };
 </script>
