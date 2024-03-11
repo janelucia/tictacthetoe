fc67ce3548d4062b996cf91c0b0927b7561ecf98
jane <janeluciaschoenfeld@gmail.com>
2024-03-11 15:54:39 +0100

    feat: add winning modal

---
+++ components/WinningModal.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="absolute bg- rounded flex flex-col items-center justify-center gap-4 w-[90vw] h-[90vw] sm:w-[50vw] sm:h-[50vw] lg:w-[40vw] lg:h-[40vw] z-10"
+    style="top: 50%; left: 50%; transform: translate(-50%, -50%)"
+  >
+    <p class="font-bold text-4xl">Player {{ player }} has won</p>
+    <button class="rounded-box bg-primary px-4 py-2">Play again!</button>
+  </div>
+</template>
--- pages/gamehub/Ultimate.vue
+++ pages/gamehub/Ultimate.vue
@@ -18,13 +18,7 @@
         <p>Player 2</p>
         <p>O</p>
       </div>
-      <div
-        v-if="field.hasWon"
-        class="absolute bg-secondary rounded flex flex-col items-center justify-center gap-4 translate-x-1/4 p-4"
-      >
-        <p class="font-bold text-4xl">Player {{ player }} has won</p>
-        <button class="rounded-box bg-primary px-4 py-2">Play again!</button>
-      </div>
+      <WinningModal v-if="field.hasWon" />
     </div>
     <div class="w-[90vw] h-[90vw] sm:w-[50vw] sm:h-[50vw] lg:w-[30vw] lg:h-[30vw]">
       <Gamefield :field="field" :player="player" @clicked-cell="clickedCell" :active-field="activeField" />
