1b79313c3833e27dbf9fa34fe08772672b566b35
jane <janeluciaschoenfeld@gmail.com>
2024-03-12 18:30:45 +0100

    add vercel speed insights

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="h-screen flex flex-col">
-    <header><NavBar /></header>
-    <main class="p-4 flex-1">
-      <slot />
-    </main>
-    <FooterBar />
-  </div>
+  <SpeedInsights />
+    <div class="h-screen flex flex-col">
+      <header><NavBar /></header>
+      <main class="p-4 flex-1">
+        <slot />
+      </main>
+      <FooterBar />
+    </div>
 </template>
+
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/nuxt';
+</script>
